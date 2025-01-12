1531640c2f2e7851190760c4610a53be4b74bca6
ridho777 <ridho.artsys@gmail.com>
2025-01-12 21:50:22 +0700

    Fix merge conflict #1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,11 +11,8 @@ import Island from "../models/Island";
 }
 
 const Home = () => {
-<<<<<<< HEAD
-=======
   const [isRotating, setIsRotating] = useState(false);
   const [currentStage, setCurrentStage] = useState(1)
->>>>>>> dda7b1c ([UPDATE] menambahkan animasi models dan menambahkan addEventListener kepada frame di component home & Menambahkan useRef, useEffect, useFrame, dan props ke model sky, island, plane, dan bird)
 
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
@@ -31,8 +28,6 @@ const Home = () => {
     return [screenScale, screenPosition, rotation];
   };
 
-<<<<<<< HEAD
-=======
   const adjustPlaneForScreenSize = () => {
     let screenScale, screenPosition;
 
@@ -47,24 +42,16 @@ const Home = () => {
     return [screenScale, screenPosition];
   };
 
->>>>>>> dda7b1c ([UPDATE] menambahkan animasi models dan menambahkan addEventListener kepada frame di component home & Menambahkan useRef, useEffect, useFrame, dan props ke model sky, island, plane, dan bird)
   const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
 
   return (
     <section className="w-full h-screen relative">
-<<<<<<< HEAD
-      <Canvas className="w-full h-screen bg-transparent" camera={{ near: 0.1, far: 1000 }}>
-=======
       <Canvas className={`w-full h-screen bg-transparent ${isRotating ? "cursor-grabbing" : "cursor-grab"}`} camera={{ near: 0.1, far: 1000 }}>
->>>>>>> dda7b1c ([UPDATE] menambahkan animasi models dan menambahkan addEventListener kepada frame di component home & Menambahkan useRef, useEffect, useFrame, dan props ke model sky, island, plane, dan bird)
         <Suspense fallback={<Loader />}>
           <directionalLight position={[1, 1, 1]} intensity={2} />
           <ambientLight intensity={0.5} />
           <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
 
-<<<<<<< HEAD
-          <Island scale={islandScale} position={islandPosition} rotation={islandRotation}/>
-=======
           <Bird />
           <Sky isRotating={isRotating}/>
           <Island 
@@ -76,7 +63,6 @@ const Home = () => {
             setCurrentStage={setCurrentStage}
           />
           <Plane planeScale={planeScale} planePosition={planePosition} isRotating={isRotating} rotation={[0, 20, 0]} />
->>>>>>> dda7b1c ([UPDATE] menambahkan animasi models dan menambahkan addEventListener kepada frame di component home & Menambahkan useRef, useEffect, useFrame, dan props ke model sky, island, plane, dan bird)
         </Suspense>
       </Canvas>
     </section>
